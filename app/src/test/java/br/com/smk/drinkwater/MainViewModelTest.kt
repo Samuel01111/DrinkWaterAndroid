beabbdf21536d1dd5e3cf5fe9cb665f01e7fb909
Samuel Nascimento <samuel.nascimento@maxmilhas.com.br>
2022-07-19 14:43:52 -0300

    develop: unit tests, changes in viewModel

--- app/src/test/java/br/com/smk/drinkwater/MainViewModelTest.kt
+++ app/src/test/java/br/com/smk/drinkwater/MainViewModelTest.kt
@@ -1,12 +1,34 @@
 package br.com.smk.drinkwater
 
-import org.junit.Test
 
-// TODO: import the test library
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
 class MainViewModelTest {
+
+    @Mock
+    private lateinit var waterCalculator: WaterCalculator
+
+    @Before
+    fun setUp() {
+
+    }
 
     @Test
     fun calculateQuantityByAge() {
+        var weight = 1
+        var age = 1
+        var isDoExercisesChecked = false
+
+        whenever(waterCalculator.calculateQuantityByAge(weight, age, isDoExercisesChecked)).thenReturn(0)
+
+        waterCalculator.calculateQuantityByAge(weight, age, isDoExercisesChecked)
+
 
     }
 
